bcdabc9eccaa7c0d8f827f7cfa71500ca9bfb4f7
reddymanyam <reddappareddy18@gmail.com>
2024-11-27 12:35:06 +0530

    QR Code Qenerator

--- QRcodeGenerator/index.js
+++ QRcodeGenerator/index.js
@@ -1,16 +1,17 @@
 let imgBox = document.getElementById("imgBox");
-        let qrImage = document.getElementById("qrImage");
-        let qrText = document.getElementById("qrText");
-        const btn = document.getElementById("btn");
+let qrImage = document.getElementById("qrImage");
+let qrText = document.getElementById("qrText");
+const btn = document.getElementById("btn");
 
-        btn.addEventListener("click",generateQR);
+btn.addEventListener("click", generateQR);
 
-        function generateQR(){
+function generateQR() {
 
-            if(qrText.value.trim().length > 0){
-                qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value.trim();
-            }else{
-                alert("enter the text")
-            }
-             console.log("button is clicked")
-        }
+    if (qrText.value.trim().length > 0) {
+        qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value.trim();
+        imgBox.classList.add("show-img");
+    } else {
+        alert("enter the text")
+    }
+    console.log("button is clicked")
+}
