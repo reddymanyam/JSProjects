8808d8ca576da1b9c3cc637faf49599a2d108338
reddymanyam <reddappareddy18@gmail.com>
2024-11-27 11:52:55 +0530

    QR Code Generator

---
+++ QRcodeGenerator/index.js
@@ -0,0 +1,16 @@
+let imgBox = document.getElementById("imgBox");
+        let qrImage = document.getElementById("qrImage");
+        let qrText = document.getElementById("qrText");
+        const btn = document.getElementById("btn");
+
+        btn.addEventListener("click",generateQR);
+
+        function generateQR(){
+
+            if(qrText.value.trim().length > 0){
+                qrImage.src = " https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value.trim();
+            }else{
+                alert("enter the text")
+            }
+             console.log("button is clicked")
+        }
