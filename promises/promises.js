72ee63c56068ad4c467e8e16d29d40788a976cf9
reddymanyam <reddappareddy18@gmail.com>
2025-01-26 02:30:07 +0530

    promise.js

--- promises/promises.js
+++ promises/promises.js
@@ -1,4 +1,15 @@
 // what is a promise (promise is an object, which will represent the eventual completion of an async operation)
+// promise has 3 states
+// 1. pending(when the async operation is not completed)
+// 2. fulfilled(resolved)
+// 3. rejected(failed)
+
+// learning outcomes
+// 1. promise (fetch method - get data from the server(API call))
+// 2. how to fetch multiple api calls (promise chaning)
+// 3. drawbacks of promise chaining (callback hell)
+// 4. how to solve the drawbacks of promise chaining (async and await) promise.all
+// 5. study about promise.allsettled
 
 //callbacks  
 /* callbacks are the functions which will pass a argument to the another function
@@ -9,15 +20,36 @@
 "https://jsonplaceholder.typicode.com/posts/2"
 */
 
-const fetchingData = () =>{
+//....................example for promise ----------------------
+let promise = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a == 2) {
+        resolve('Success');
+    } else {   
+        reject('Failed');
+    }                
+});
 
-    
-    fetch("https://jsonplaceholder.typicode.com/posts/1").then((data)=>{ 
-           alert(`the data we are getting is :${data.title} `)
-     }).catch((err)=>{
-           alert(`https://the err is :${err}`)
-     })
+promise.then((message) => {
+    console.log('This is in the then ' + message);
+}).catch((message) => {
+    console.log('This is in the catch ' + message);
+});
 
+//....................example for promise real world API'S ----------------------
+const getData = () => {
+  
+    fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>{
+        
+        if(!response.ok){
+            throw new error(`https:// getting error ${response.status}`)
+        }
+        const data = response.json()
+        console.log(data)
+        return data.title;
+
+    }).catch((error)=>{
+       alert(`https://www.getting error is ${error.message}`)
+    })
 }
-
-fetchingData();
+getData();
